215bb0e11b61f47f8bec2975b34cbfdf466bbadb
Savio Tsui <stsui@splunk.com>
2020-08-01 02:38:21 -0700

    fixing icons using important css

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,27 +8,29 @@ import Image from "../components/image";
 
 const imageDimension = 24;
 const IconSection = styled(Section)`
-    display: flex;
-    justify-content: space-between;
+    display: flex !important;
+    justify-content: space-between !important;
 `;
 
 export default function Home() {
-    const centerContent = (
-        <CenteredLayout>
-            <Section>
-                <Header headerText="Savio Tsui" />
-                <IconSection>
-                    <a href="https://www.splunk.com/" target="_blank" rel="noreferrer"><Image src="/assets/splunk/splunk-logo.png" alt="Splunk, Inc." height={imageDimension} width={imageDimension}></Image></a>
-                    <a href="https://github.com/saviotsui/" target="_blank" rel="noreferrer"><Image src="/assets/github/github-logo.png" alt="Github, Inc." height={imageDimension} width={imageDimension}></Image></a>
-                    <a href="https://www.linkedin.com/in/saviotsui/" target="_blank" rel="noreferrer"><Image src="/assets/linkedin/linkedin-logo.png" alt="Linkedin Corp." height={imageDimension} width={imageDimension}></Image></a>
-                    <a href="https://www.strava.com/athletes/2815426" target="_blank" rel="noreferrer"><Image src="/assets/strava/strava-logo.png" alt="Strava, Inc." height={imageDimension} width={imageDimension}></Image></a>
-                </IconSection>
-            </Section>
-        </CenteredLayout>
-    );
+    const centerContent = () => {
+        return (
+            <CenteredLayout>
+                <Section>
+                    <Header headerText="Savio Tsui" />
+                    <IconSection>
+                        <a href="https://www.splunk.com/" target="_blank" rel="noreferrer"><Image src="/assets/splunk/splunk-logo.png" alt="Splunk, Inc." height={imageDimension} width={imageDimension}></Image></a>
+                        <a href="https://github.com/saviotsui/" target="_blank" rel="noreferrer"><Image src="/assets/github/github-logo.png" alt="Github, Inc." height={imageDimension} width={imageDimension}></Image></a>
+                        <a href="https://www.linkedin.com/in/saviotsui/" target="_blank" rel="noreferrer"><Image src="/assets/linkedin/linkedin-logo.png" alt="Linkedin Corp." height={imageDimension} width={imageDimension}></Image></a>
+                        <a href="https://www.strava.com/athletes/2815426" target="_blank" rel="noreferrer"><Image src="/assets/strava/strava-logo.png" alt="Strava, Inc." height={imageDimension} width={imageDimension}></Image></a>
+                    </IconSection>
+                </Section>
+            </CenteredLayout>
+        );
+    };
 
     return (
-        <ThreeColLayout centerContent={centerContent}>
+        <ThreeColLayout centerContent={centerContent()}>
         </ThreeColLayout>
     );
 }
