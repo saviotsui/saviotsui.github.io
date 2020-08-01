f76968c16abbe227ffe3cc2c41c0b6531e494cfd
Savio Tsui <stsui@splunk.com>
2020-08-01 02:21:10 -0700

    fixing index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,11 +6,6 @@ import Section from '../layouts/section';
 import Header from "../components/header";
 import Image from "../components/image";
 
-const Body = styled(Section)`
-    color: white;
-    width: 100%;
-`;
-
 const imageDimension = 24;
 const IconSection = styled(Section)`
     display: flex;
@@ -22,14 +17,12 @@ export default function Home() {
         <CenteredLayout>
             <Section>
                 <Header headerText="Savio Tsui" />
-                <Body>
-                    <IconSection>
-                        <a href="https://www.splunk.com/" target="_blank" rel="noreferrer"><Image src="/assets/splunk/splunk-logo.png" alt="Splunk, Inc." height={imageDimension} width={imageDimension}></Image></a>
-                        <a href="https://github.com/saviotsui/" target="_blank" rel="noreferrer"><Image src="/assets/github/github-logo.png" alt="Github, Inc." height={imageDimension} width={imageDimension}></Image></a>
-                        <a href="https://www.linkedin.com/in/saviotsui/" target="_blank" rel="noreferrer"><Image src="/assets/linkedin/linkedin-logo.png" alt="Linkedin Corp." height={imageDimension} width={imageDimension}></Image></a>
-                        <a href="https://www.strava.com/athletes/2815426" target="_blank" rel="noreferrer"><Image src="/assets/strava/strava-logo.png" alt="Strava, Inc." height={imageDimension} width={imageDimension}></Image></a>
-                    </IconSection>
-                </Body>
+                <IconSection>
+                    <a href="https://www.splunk.com/" target="_blank" rel="noreferrer"><Image src="/assets/splunk/splunk-logo.png" alt="Splunk, Inc." height={imageDimension} width={imageDimension}></Image></a>
+                    <a href="https://github.com/saviotsui/" target="_blank" rel="noreferrer"><Image src="/assets/github/github-logo.png" alt="Github, Inc." height={imageDimension} width={imageDimension}></Image></a>
+                    <a href="https://www.linkedin.com/in/saviotsui/" target="_blank" rel="noreferrer"><Image src="/assets/linkedin/linkedin-logo.png" alt="Linkedin Corp." height={imageDimension} width={imageDimension}></Image></a>
+                    <a href="https://www.strava.com/athletes/2815426" target="_blank" rel="noreferrer"><Image src="/assets/strava/strava-logo.png" alt="Strava, Inc." height={imageDimension} width={imageDimension}></Image></a>
+                </IconSection>
             </Section>
         </CenteredLayout>
     );
